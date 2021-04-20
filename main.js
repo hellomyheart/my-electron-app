cb22b14ef0de9a6ad0dd1ed38bbf12fc8f636075
stephenshen <723809534@qq.com>
2021-04-20 14:04:32 +0800

    设置任务栏缩略图&清除缩略图

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+function createWindow() {
+  const win = new BrowserWindow()
 
   win.loadFile('index.html')
+
+  //设置缩略图
+  win.setThumbarButtons([
+    {
+      tooltip: 'button1',
+      icon: path.join(__dirname, 'test.png'),
+      click() { console.log('button1 clicked') }
+    }, {
+      tooltip: 'button2',
+      icon: path.join(__dirname, 'test2.png'),
+      // flags: ['enabled', 'dismissonclick'],
+      click() { console.log('button2 clicked.') }
+    }
+  ])
+
+
+  // 清除缩略图
+  win.setThumbarButtons([])
+
 }
+
+
 
 app.whenReady().then(() => {
   createWindow()
