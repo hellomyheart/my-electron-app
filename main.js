2f2e0ea6a626e81f397537648a1ddecb1fc68555
stephenshen <723809534@qq.com>
2021-04-20 23:03:16 +0800

    闪烁框

--- main.js
+++ main.js
@@ -2,14 +2,10 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
+  const win = new BrowserWindow()
+  // 闪烁框，好像没有效果
+  win.once('focus', () => win.flashFrame(false))
+  win.flashFrame(true)
   win.loadFile('index.html')
 }
 
